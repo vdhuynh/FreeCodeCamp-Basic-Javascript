2fa8369c8d71ede4087e96e57fc5233ccca72b30
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-06-15 05:48:14 -0500

    Create escape_sequences.js

---
+++ escape_sequences.js
@@ -0,0 +1,10 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \t	tab
+// \r	carriage return
+// \b	backspace
+// \f	form feed
