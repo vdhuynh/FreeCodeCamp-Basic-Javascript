75b0aa533e935a320d904d5afc06bd2d42844706
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-06-19 10:36:05 -0500

    Create manipulating_complex_objects.js

---
+++ manipulating_complex_objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true,
+  },
+  {
+    "artist": "Yngwie Malmsteen",
+    "title": "Trilogy Suite",
+    "release_year": 1986,
+    "formats": [
+      "CD",
+      "LP",
+      "Spotify"
+    ],
+  }
+];
